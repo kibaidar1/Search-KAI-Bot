0bfd54ef99361d1738007fcf305dda86a05e7bdf
kibaidar1 <aidar1996@gmail.com>
2019-06-10 21:42:16 +0500

    first deploy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Search-KAI-Bot',
+    version='1.0',
+    packages=['Botegram'],
+    url='',
+    license='',
+    author='kibaidar1',
+    author_email='',
+    description=''
+)
